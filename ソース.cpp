f6703b73507701e636083314b76291a4845f13cb
hmj2622024Otsuki <hmj2622024@stu.o-hara.ac.jp>
2026-07-03 16:50:50 +0900

    プログラムの内容を大幅に変更

--- ソース.cpp
+++ ソース.cpp
@@ -13,10 +13,11 @@ int APIENTRY WinMain(
 	const int WIDTH = 960, HEIGHT = 640; // ウィンドウの幅と高さのピクセル数
 	const int WHITE = GetColor(255, 255, 255); // 白
 	const int RED = GetColor(255, 0, 0); // 赤
+	const int BLACK = GetColor(0, 0, 0); // 黒
 
-	// 関数の宣言
-	int timer = 0;
-
+	float jumpPower = -20.0f;
+	float gravity = 1.8f;
+	bool jump = false;
 
 	SetWindowText("Run"); // ウィンドウのタイトル
 
@@ -26,17 +27,79 @@ int APIENTRY WinMain(
 	SetBackgroundColor(0, 255, 0); // 背景色の指定
 	SetDrawScreen(DX_SCREEN_BACK); // 描画面を裏画面にする
 
+	int timer = 0;
+
+	// 画像の用意
+	int imgPlayer[4] = { // 配列にプレイヤー(女の子)の画像を読み込む
+		LoadGraph("image/run01.png"),
+		LoadGraph("image/run02.png"),
+		LoadGraph("image/run03.png"),
+		LoadGraph("image/run04.png")
+	};
+	int imgPlayerJump = LoadGraph("image/jump.png"); // プレイヤーのジャンプ画像を読み込む
+	int imgBG = LoadGraph("image/bg.png"); // 変数に背景画像を読み込む
+	int imgGround = LoadGraph("image/ground.png"); // 変数に地面の画像を読み込む
+
+	// 効果音・BGMの用意
+	int jumpSE = LoadSoundMem("sound/jump.mp3");
+	int overSE = LoadSoundMem("sound/over.mp3");
+
+	// プレイヤーの座標用の変数
+	float playerX = 200, playerY = 400;
+	float playerVY = 0.0f;
+
 	while (1)
 	{
-		ClearDrawScreen(); // 図形をマウスポインタの位置にする
-		timer++;
+		ClearDrawScreen(); // 画面をクリアする
 
-		int size = 200;
-		SetFontSize(size);
-		if (timer % 30 < 15)
+		// 背景
+		int spd = 1; // スクロール速度の初期値
+		static int bgX;
+		bgX = (bgX - spd * 2) % WIDTH;
+		DrawGraph(bgX + WIDTH, 0, imgBG, false); // 背景の表示
+		DrawGraph(bgX, 0, imgBG, false); //
+
+		// 地面
+		static int groundX;
+		groundX = (groundX - spd * 9) % WIDTH;
+		DrawGraph(groundX + WIDTH, 0, imgGround, true); // 地面の表示
+		DrawGraph(groundX, 0, imgGround, true); //
+
+		// プレイヤー
+		if (jump == true)
 		{
-			DrawString(WIDTH / 2 - size - 50, HEIGHT / 2 - size + 100, "アホ！", RED);
+			DrawGraph(playerX, playerY, imgPlayerJump, true);
 		}
+		else
+		{
+			DrawGraph(playerX, playerY, imgPlayer[(timer / 3) % 4], true); // プレイヤーの表示
+		}
+
+		timer++; // 時間のカウント
+		DrawFormatString(0, 0, BLACK, "%d", timer);
+
+		// ジャンプの処理
+		if (CheckHitKey(KEY_INPUT_SPACE) == 1 && !jump)
+		{
+			playerVY = jumpPower;
+			jump = true;
+			PlaySoundMem(jumpSE, DX_PLAYTYPE_BACK);
+		}
+
+		if (jump == true)
+		{
+			playerVY += gravity;
+			playerY += playerVY;
+
+			if (playerY >= 400.0f)
+			{
+				playerY = 400.0f;
+				playerVY = 0.0f;
+				jump = false;
+			}
+		}
+
+
 
 		ScreenFlip(); // 裏画面の内容を表画面に反映させる
 		WaitTimer(33); // 一定時間待つ
